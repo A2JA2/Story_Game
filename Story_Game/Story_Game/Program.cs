fd41bcfd9806307056a91c68b950c0f437ab9c4f
A2JA2 <55232721+A2JA2@users.noreply.github.com>
2020-01-23 12:09:51 +0000

    Add files via upload

---
+++ Story_Game/Story_Game/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("You're walking down a street.\nyou see a bear, do you run Y/N?\n");
+            string D1 = Console.ReadLine();
+            if(D1 == "Y")
+                {
+                Console.Write("You live another day!\n");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Write("You're are dead - Game Over");
+                Console.ReadKey();
+            }
+            Console.Write("are you here? Y/N\n");
+        }
+        
+    }
+    
+}
